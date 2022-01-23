f8f23fd090f062c4f6140c5a396240de2ed14b82
ProIvanko <ivanko1432@gmail.com>
2022-01-23 23:29:28 +0300

    useCallback added

--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
-//
+//useCallback в отличие от useMemo возвращает не ЗНАЧЕНИЕ функции, а саму функцию (либо предотвращает рендер)
 
 import React, {useState} from "react";
+import { useCallback } from "react/cjs/react.development";
+import ItemsList from "./itemslist";
 
 function App() {
 
   const [colored, setColored] = useState(false)
-
   const [count, setCount] = useState(1)
 
   const styles = {
     color: colored ? 'blue' : 'black'
   }
 
+  const generateItemsFromAPI = useCallback ((index) => {
+    return new Array(count).fill('').map((_, i) => `Element ${i + index}`)
+  }, [count])
+
   return (
     <>
-      <h1 style={styles} >Quantity:</h1>
+      <h1 style={styles} >Quantity: {count} </h1>
 
       <button className="btn btn-success" onClick={() => setCount(prev => prev + 1)} >Add</button>
 
-      <button className="btn btn-success" onClick={() => setCount(prev => prev - 1)} >Remove</button>
+      <button className="btn btn-warning" onClick={() => setColored(prev => !prev)} >Change color</button>
+
+      <ItemsList getItems =  {generateItemsFromAPI}/>
 
     </>
   )
