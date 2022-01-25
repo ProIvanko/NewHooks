79901e094038344cf8b8b626c21648a9f8ff6b78
ProIvanko <ivanko1432@gmail.com>
2022-01-25 12:35:15 +0300

    useContext Hook

--- src/main.js
+++ src/main.js
@@ -2,11 +2,11 @@ import React from "react";
 import { useAlert } from "./alert/AlertContext";
 
 export default function Main() {
-    const {toggle} = useAlert()
+    const {show} = useAlert()
     return (
         <>
             <h1>Hello, Context World!</h1>
-            <button onClick = {toggle} className="btn btn-success" >Show alert</button>
+            <button onClick = {() => show('This text from Main.js')} className="btn btn-success" >Show alert</button>
         </>
     )
 }
